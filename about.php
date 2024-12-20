a23d22187c0995b96e062b32d9099aa172c616bc
Hema Bodapoori <87563626+Hema1854@users.noreply.github.com>
2024-12-20 20:13:38 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,64 @@
+<?php include 'header.php' ?>
+
+<div class="breadcumb-wrapper" data-bg-src="assets/img/bg/breadcumb-bg.jpg">
+    <div class="container">
+        <div class="breadcumb-content">
+            <h1 class="breadcumb-title">About Us</h1>
+            <ul class="breadcumb-menu">
+                <li><a href="index.php">Home</a></li>
+                <li>About Us</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="overflow-hidden space">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-6 mb-30  mb-xl-0">
+                <div class="img-box4 tilt-active">
+                    <div class="img-shape icon-masking"><span class="mask-icon"
+                            data-mask-src="assets/img/normal/about_3_1-shape.png"></span> <img
+                            src="assets/img/normal/about_3_1-shape.png" alt="img"></div><img
+                        src="assets/img/aboutappx.png" alt="About">
+                </div>
+            </div>
+            <div class="col-xl-6 mt-40 mt-xl-0">
+                <div class="title-area mb-35 text-center text-xl-start">
+                    <div class="shadow-title">ABOUT US</div><span class="sub-title">
+                        <div class="icon-masking me-2"><span class="mask-icon"
+                                data-mask-src="assets/img/theme-img/title_shape_2.svg"></span> <img
+                                src="assets/img/theme-img/title_shape_2.svg" alt="shape"></div>About AppXcelerate
+                        Solutions,
+                    </span>
+                    <h4 class="sec-title">Welcome To  AppXcelerate Solutions: Your Comprehensive Hub For <span
+                            class="text-theme">CRM And IT
+                            Services.</span></h4>
+                </div>
+                <p class="mt-n2 mb-30 text-center text-xl-start">
+                    At  AppXcelerate Solutions, we champion the seamless integration of efficient customer relationship
+                    management (CRM) with a spectrum of IT services, forming a holistic approach to elevate your
+                    business. As trusted CRM specialists, we offer a comprehensive suite of solutions tailored to not
+                    only enhance your customer relationships but also propel your business into the realms of digital
+                    transformation and innovation.
+
+                </p>
+
+                <p>
+                     AppXcelerate Solutions stands as your beacon for comprehensive IT services, spearheading your digital
+                    transformation initiatives and bespoke app development projects. In the ever-evolving digital
+                    landscape, staying ahead requires more than just efficient customer management – it demands a
+                    strategic approach to technology. Our IT services are crafted to make your business more agile,
+                    responsive, and technologically advanced. From conceptualizing and designing digital transformation
+                    strategies to developing customized applications,  AppXcelerate Solutions is your gateway to an
+                    innovative and tech-driven future.
+
+                </p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php' ?>
+
